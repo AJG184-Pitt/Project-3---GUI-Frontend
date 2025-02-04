bfb22489bb294e064a35f5ed81370640d0bcdb4e
AJG184-Pitt <AJG184@pitt.edu>
2025-02-04 09:13:26 -0500

    updates on bundles

--- conductor.py
+++ conductor.py
@@ -2,6 +2,8 @@ class Conductor:
     def __init__(self, name, diam, GMR, resistance, ampacity):
         self.name = name
         self.diam = diam
+        self.radius = diam / 24
         self.GMR = GMR
         self.resistance = resistance
+        self.resistance_c = self.resistance_c
         self.ampacity = ampacity

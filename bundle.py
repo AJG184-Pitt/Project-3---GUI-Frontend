43ee640c6115f3ba4c1d14a940e0a2b4baed3173
AJG184-Pitt <AJG184@pitt.edu>
2025-02-04 08:32:36 -0500

    calculated bundle elements

--- bundle.py
+++ bundle.py
@@ -1,3 +1,5 @@
+import math
+
 class Bundle:
     def __init__(self, name, num_conductors, spacing, conductor):
         self.name = name
@@ -7,4 +9,10 @@ class Bundle:
         self.DSC, self.DSL = self.calc_radii()
 
     def calc_radii(self):
-        return
+        if self.num_conductor == 1:
+            self.DSC = 0
+        else:
+            self.DSC = self.spacing
+
+        self.DSL = self.spacing / (2 * math.sin(math.pi/self.num_conductors))
+        return self.DSC, self.DSL

b81b5f89f07c3c2825fc56cefd9b8413c4c0b998
Aidan Gresko <AJG184@pitt.edu>
2025-02-06 08:11:32 -0500

    Update geometry.py
    Updated math to np library

--- geometry.py
+++ geometry.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 class Geometry:
     def __init__(self, name, xa, ya, xb, yb, xc, yc):
@@ -14,8 +14,8 @@ class Geometry:
     # Source: https://www.cuemath.com/distance-formula/
 
     def calc_Deq(self):
-        dab = math.sqrt((self.xb**2 - self.xa**2) + (self.yb**2 - self.ya**2))
-        dbc = math.sqrt((self.xc**2 - self.xb**2) + (self.yc**2 - self.yb**2))
-        dac = math.sqrt((self.xc**2 - self.xa**2) + (self.yc**2 - self.ya**2))
+        dab = np.sqrt((self.xb**2 - self.xa**2) + (self.yb**2 - self.ya**2))
+        dbc = np.sqrt((self.xc**2 - self.xb**2) + (self.yc**2 - self.yb**2))
+        dac = np.sqrt((self.xc**2 - self.xa**2) + (self.yc**2 - self.ya**2))
 
         return (dab + dbc + dac) / 3

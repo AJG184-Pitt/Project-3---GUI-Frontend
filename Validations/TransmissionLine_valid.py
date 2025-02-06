6d64e1613b05092c5e8a0089be3c9920cecf295b
Aidan Gresko <AJG184@pitt.edu>
2025-02-06 09:05:23 -0500

    My changes

--- Validations/TransmissionLine_valid.py
+++ Validations/TransmissionLine_valid.py
@@ -5,4 +5,3 @@ from conductor import Conductor
 conductor1 = Conductor("Partridge", 0.642, 0.0217, 0.385, 460)
 bundle1 = Bundle("Bundle 1", 2, 1.5, conductor1)
 transmission1 = TransmissionLine("T1", "B1", "B2", 2, )
-

1a01cf09c378a3bf624acad7be63b6b3c686b83a
Aidan Gresko <AJG184@pitt.edu>
2025-02-06 09:34:29 -0500

    Update TransmissionLine_valid.py

--- Validations/TransmissionLine_valid.py
+++ Validations/TransmissionLine_valid.py
@@ -1,11 +1,15 @@
 from transmissionline import TransmissionLine
+from bus import Bus
+from bundle import Bundle
+from conductor import Conductor
+from geometry import Geometry
 
-line1 = TransmissionLine("Line 1", "bus1","bus2", TransmissionLine.bundle, TransmissionLine.geometry, 10)
+bus1 = Bus("Bus_1", 180)
+bus2 = Bus("Bus_2", 230)
+conductor1 = Conductor("Partridge", 0.642, 0.0217, 0.385, 460)
+bundle1 = Bundle("Bundle 1", 2, 1.5, conductor1)
+geometry1 = Geometry("Geometry 1", 0, 0, 18.5, 0,37, 0)
 
+line1 = TransmissionLine("Line 1", bus1, bus2, bundle1, geometry1, 10)
 
-print(line1.name, line1.bus1.name, line1.bus2.name, line1.length)
-<<<<<<< HEAD
-print(line1.zseries, line1.bseries, line1.yseries)
-=======
-print(line1.zseries, line1.bseries, line1.yseries)
->>>>>>> main
+print(f"Line: {line1.name}, bus1: , bus2: , bundle1 : geometry1: {line1.geometry}, geometry: {line1.length}")

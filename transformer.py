873e92b9cff494ce1357bad56a183e7133823aeb
Aidan Gresko <AJG184@pitt.edu>
2025-02-06 08:40:48 -0500

    Fixed indentation

--- transformer.py
+++ transformer.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class Transformer:
     def __init__(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio):
         self.name = name
@@ -7,19 +8,19 @@ class Transformer:
         self.power_rating = power_rating
         self.impedance_percent = impedance_percent
         self.x_over_r_ratio = x_over_r_ratio
-        self.zpu = self.calc_impedance()
+        self.zt = self.calc_impedance()
         self.yt = self.calc_admittance()
         self.yprim = None
 
-        def calc_impedance(self):
-            self.theta = np.atan(self.x_over_r_ratio)
-            self.zpu = self.impedance_percent/100
-            self.xpu = self.zpu/ np.sin(self.theta)
-            self.rpu = self.zpu / np.cos(self.theta)
-            return self.zpu
+    def calc_impedance(self):
+        self.theta = np.atan(self.x_over_r_ratio)
+        self.zpu = self.impedance_percent/100
+        self.xpu = self.zpu/ np.sin(self.theta)
+        self.rpu = self.zpu / np.cos(self.theta)
+        return self.zt
 
-        def calc_admittance(self):
-            if self.zpu != 0:
-                return 1 / self.zpu
+    def calc_admittance(self):
+        if self.zt != 0:
+                return 1 / self.zt
             else:
                 return 0

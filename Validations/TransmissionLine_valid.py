3ba7d47aa793273b209231abcab7a1a1700b685b
jpk102pitt <jpk102@pitt.edu>
2025-02-06 08:59:56 -0500

    did some validation

--- Validations/TransmissionLine_valid.py
+++ Validations/TransmissionLine_valid.py
@@ -4,5 +4,4 @@ line1 = TransmissionLine("Line 1", "bus1","bus2", TransmissionLine.bundle, Trans
 
 
 print(line1.name, line1.bus1.name, line1.bus2.name, line1.length)
-print(line1.zbase, line1.ybase)
 print(line1.zseries, line1.bseries, line1.yseries)

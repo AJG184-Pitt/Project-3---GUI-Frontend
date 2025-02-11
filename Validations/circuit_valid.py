ba80522612e19cb0a0ed0e24af7c341168d21811
Aidan Gresko <AJG184@pitt.edu>
2025-02-11 08:26:58 -0500

    added more validations

--- Validations/circuit_valid.py
+++ Validations/circuit_valid.py
@@ -1,15 +1,14 @@
 import unittest
 
+# Import classes
 import bus
 from circuit import Circuit
-from bus import Bus
 
 class TestMethods(unittest.TestCase):
     def setUp(self):
         self.circuit = Circuit("Test Circuit")
 
     def test_circuit_buses(self):
-
         output1 = self.circuit.name
         expected1 = "Test Circuit"
         self.assertEqual(output1, expected1)
@@ -25,6 +24,46 @@ class TestMethods(unittest.TestCase):
         output4 = type(self.circuit.buses)
         expected4 = dict
         self.assertEqual(output4, expected4)
+
+        output5 = self.circuit.bundles
+        expected5 = {}
+        self.assertEqual(output5, expected5)
+
+        output6 = type(self.circuit.bundles)
+        expected6 = dict
+        self.assertEqual(output6, expected6)
+
+        output7 = self.circuit.conductors
+        expected7 = {}
+        self.assertEqual(output7, expected7)
+
+        output8 = type(self.circuit.conductors)
+        expected8 = dict
+        self.assertEqual(output8, expected8)
+
+        output9 = self.circuit.geometries
+        expected9 = {}
+        self.assertEqual(output9, expected9)
+
+        output10 = type(self.circuit.geometries)
+        expected10 = dict
+        self.assertEqual(output10, expected10)
+
+        output11 = self.circuit.geometries
+        expected11 = {}
+        self.assertEqual(output11, expected11)
+
+        output12 = type(self.circuit.geometries)
+        expected12 = dict
+        self.assertEqual(output12, expected12)
+
+        output13 = self.circuit.transmission_lines
+        expected13 = {}
+        self.assertEqual(output13, expected13)
+
+        output14 = type(self.circuit.transmission_lines)
+        expected14 = dict
+        self.assertEqual(output14, expected14)
 
     def test_bus_components(self):
         self.circuit.add_bus("Bus1", 230)

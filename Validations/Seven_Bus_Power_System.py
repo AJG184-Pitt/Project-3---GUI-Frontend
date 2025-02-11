54af6eed4f040e328f516564476b4795dca2824b
jpk102pitt <jpk102@pitt.edu>
2025-02-11 18:47:17 -0500

    added new components to system file

--- Validations/Seven_Bus_Power_System.py
+++ Validations/Seven_Bus_Power_System.py
@@ -11,18 +11,24 @@ circuit1.add_bus("Bus5", 230)
 circuit1.add_bus("Bus6", 230)
 circuit1.add_bus("Bus7", 18)
 
-#adding the transformers
+#adding the 2 transformers
 circuit1.add_transformer("T1", "Bus1", "Bus2", 125, 8.5, 10 )
 circuit1.add_transformer("T2", "Bus6", "Bus7", 200, 10.5, 12 )
 
 #adding the conductor
 circuit1.add_conductor("C1",.642, .0217, .385, 460)
+
 #adding the bundle
 circuit1.add_bundle("B1",2, 1.5, "C1")
-#adding the geometry
-#geometry_obj = Geometry(name, xa, ya, xb, yb, xc, yc)
-circuit1.add_geometry( "G1", )
 
-#adding the transmission lines
-#transmission_line_obj = TransmissionLine(name, bus1, bus2, bundle, conductor, geometry, length,f)
+#adding the geometry
+circuit1.add_geometry( "G1", 0, 0, 0, 0, 0, 0) # ask about how to find x and y values
+
+#adding the 6 transmission lines
+circuit1.add_transmission_line("L1", "Bus2", "Bus4", "B1", "C1", "G1", 10, 60)
+circuit1.add_transmission_line("L2", "Bus2", "Bus3", "B1", "C1", "G1", 25, 60)
+circuit1.add_transmission_line("L3", "Bus3", "Bus5", "B1", "C1", "G1", 20, 60)
+circuit1.add_transmission_line("L4", "Bus4", "Bus6", "B1", "C1", "G1", 20, 60)
+circuit1.add_transmission_line("L5", "Bus5", "Bus6", "B1", "C1", "G1", 10, 60)
+circuit1.add_transmission_line("L6", "Bus4", "Bus5", "B1", "C1", "G1", 35, 60)
 

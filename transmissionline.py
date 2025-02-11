3235bd96f492088844ecf89e31f19e87bb43d77e
Aidan Gresko <AJG184@pitt.edu>
2025-02-11 09:33:00 -0500

    Fixed some of the classes partially

--- transmissionline.py
+++ transmissionline.py
@@ -1,6 +1,11 @@
 import numpy as np
+from bundle import Bundle
+from conductor import Conductor
+from geometry import Geometry
+
+
 class TransmissionLine:
-    def __init__(self, name, bus1, bus2, bundle, conductor, geometry, length):
+    def __init__(self, name: str, bus1: str, bus2: str, bundle: Bundle, conductor: Conductor, geometry: Geometry, length: float):
         self.name = name
         self.bus1 = bus1
         self.bus2 = bus2

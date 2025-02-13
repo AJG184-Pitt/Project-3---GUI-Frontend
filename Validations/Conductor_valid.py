c7305a04582b67f3d39aa138d9e3258eb44f86cb
Aidan Gresko <AJG184@pitt.edu>
2025-02-13 09:16:55 -0500

    moved test cases into classes

--- Validations/Conductor_valid.py
+++ Validations/Conductor_valid.py
@@ -1,5 +1,2 @@
 from conductor import Conductor
 
-conductor1 = Conductor("Partridge", 0.642, 0.0217, 0.385, 460)
-
-print(f"name: {conductor1.name}, diam: {conductor1.diam},GMR: , resistance: {conductor1.resistance}, ampacity: {conductor1.ampacity}")

7caabb33e7e51079d6c7fdb78ee9f82207393a23
Aidan Gresko <AJG184@pitt.edu>
2025-02-17 21:29:18 -0500

    Checked the outputs of new calculations

--- transformer.py
+++ transformer.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 
     transformer1 = Transformer("T1", bus1, bus2, 125, 8.5, 10)
 
-    print(
-        f"{transformer1.name} {transformer1.bus1}, {transformer1.bus2}, {transformer1.power_rating}, {transformer1.impedance_percent}, {transformer1.x_over_r_ratio}")
+    print(f"{transformer1.name} {transformer1.bus1}, {transformer1.bus2}, {transformer1.power_rating}, {transformer1.impedance_percent}, {transformer1.x_over_r_ratio}")
     print(transformer1.calc_admittance())
     print(transformer1.calc_impedance())
+    print(transformer1.yprim)

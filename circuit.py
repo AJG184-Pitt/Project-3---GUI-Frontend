58bbdbda4086cd40f9fa23ccf1d055a61d4ae324
Aidan Gresko <AJG184@pitt.edu>
2025-02-25 09:29:48 -0500

    worked on ybus matrix in circuit class

--- circuit.py
+++ circuit.py
@@ -4,6 +4,7 @@ from conductor import Conductor
 from geometry import Geometry
 from transformer import Transformer
 from transmissionline import TransmissionLine
+import numpy as np
 
 class Circuit:
     def __init__(self, name: str):
@@ -14,6 +15,7 @@ class Circuit:
         self.geometries = dict()
         self.transformers = dict()
         self.transmission_lines = dict()
+        self.ybus = None
 
     def add_bundle(self, name, num_conductors, spacing, conductor):
         bundle_obj = Bundle(name, num_conductors, spacing, self.conductors[conductor])
@@ -38,3 +40,29 @@ class Circuit:
     def add_transmission_line(self, name, bus1, bus2, bundle, conductor, geometry, length):
         transmission_line_obj = TransmissionLine(name, self.buses[bus1], self.buses[bus2], self.bundles[bundle], self.conductors[conductor], self.geometries[geometry], length)
         self.transmission_lines[name] = transmission_line_obj
+
+    def calc_ybus(self):
+        N = len(self.buses)
+
+        # Initialize ybus matrix
+        Ybus = np.zeros((N, N), dtype=complex)
+
+        # Iterate through components
+        for component in self.transformers + self.transmission_lines:
+            Yprim = component.yprim
+            bus1_index = self.buses.index(component.bus1)
+            bus2_index = self.buses.index(component.bus2)
+
+            # Add self-admittances
+            Ybus[bus1_index, bus1_index] += Yprim[0,0]
+            Ybus[bus2_index, bus2_index] += Yprim[1,1]
+
+            # Mutual admittances
+            Ybus[bus1_index, bus2_index] += Yprim[0,1]
+            Ybus[bus2_index, bus1_index] += Yprim[1,0]
+
+        for i in range(N):
+            if Ybus[i, i] == 0:
+                raise ValueError(f"Bus {self.buses[i].name} has no self-admittance")
+
+        self.ybus = Ybus

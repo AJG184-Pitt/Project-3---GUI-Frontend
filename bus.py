30d729692c6fab3d0e75729183201fdcfca18d6c
Aidan Gresko <AJG184@pitt.edu>
2025-02-24 21:31:27 -0500

    Added some small tests for new classes

--- bus.py
+++ bus.py
@@ -3,7 +3,7 @@ import warnings
 class Bus:
     bus_count = 0
 
-    def __init__(self, name: str, base_kv: float, bus_type, vpu=1.0, delta=0.0):
+    def __init__(self, name: str, base_kv: float, bus_type: str, vpu=1.0, delta=0.0):
         self.name = name
         self.base_kv = base_kv
         self.index = Bus.bus_count
@@ -22,6 +22,6 @@ if __name__ == '__main__':
     bus1 = Bus("Bus 1", 20, "PV Bus")
     bus2 = Bus("Bus 2", 230, "PQ Bus")
 
-    print(f"Bus 1: {bus1.name}, {bus1.base_kv}, {bus1.index}")
-    print(f"Bus 2: {bus2.name}, {bus2.base_kv}, {bus2.index}")
+    print(f"Bus 1: {bus1.name}, {bus1.base_kv}, {bus1.index}, {bus1.vpu}, {bus1.delta}")
+    print(f"Bus 2: {bus2.name}, {bus2.base_kv}, {bus2.index}, {bus2.vpu}, {bus2.delta}")
     print(f"Bus count: {Bus.bus_count}")

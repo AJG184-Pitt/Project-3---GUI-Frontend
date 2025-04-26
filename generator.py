bae8b210dc8df5a3df90db21440badf6ecc635b4
jpk102pitt <jpk102@pitt.edu>
2025-04-26 16:02:26 -0400

    jake

--- generator.py
+++ generator.py
@@ -1,12 +1,22 @@
+from numpy.linalg.lapack_lite import zgelsd
+
 from bus import Bus
 
 class Generator:
 
-    def __init__(self, name: str, bus: Bus, voltage_setpoint: float, mw_setpoint: float):
+    def __init__(self, name: str, bus: Bus, voltage_setpoint: float, mw_setpoint: float, x1 , x2, x0, zg ):
         self.name = name
         self.bus = bus
         self.voltage_setpoint = voltage_setpoint
         self.mw_setpoint = mw_setpoint
+        self.x1 = x1
+        self.x2 = x2
+        self.x0 = x0
+        self.zg = zg
+        self.y1 = 1/(1j*x1)
+        self.y2 = 1/(1j*x2)
+        self.y0 = 1/(1j*x0+3*zg)
+
 
 if __name__ == '__main__':
     bus = Bus("Bus 1", 20, "PV Bus")
